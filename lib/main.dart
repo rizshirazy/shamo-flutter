cbf97ba9996025cafbbe5ca20c584819b728b1cb
Muhammad Rizki <hammadri@MacBook-Pro-Muhammad.local>
2022-03-25 10:55:51 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/splash_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': ((context) => SignInPage())
+      },
+    );
+  }
+}
