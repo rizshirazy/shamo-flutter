4e1cee00213e61292a985fbefa9dcbc11e9adcd0
Muhammad Rizki <rizkielshirazy@gmail.com>
2022-03-26 21:38:39 +0700

    slicing chat with product preview

---
+++ lib/pages/detail_chat_page.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class DetailChatPage extends StatelessWidget {
+  const DetailChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PreferredSizeWidget header() {
+      return PreferredSize(
+        child: Container(
+          margin: EdgeInsets.only(
+            top: 10,
+          ),
+          child: AppBar(
+            backgroundColor: bgColor1,
+            centerTitle: false,
+            title: Row(
+              children: [
+                Image.asset(
+                  'assets/image_shop_logo_online.png',
+                  width: 50,
+                ),
+                SizedBox(
+                  width: 12,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Shoe Store',
+                      style: primaryTextStyle.copyWith(
+                        fontWeight: medium,
+                      ),
+                    ),
+                    Text(
+                      'online',
+                      style: secondaryTextStyle.copyWith(
+                          fontWeight: light, fontSize: 14),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(80),
+      );
+    }
+
+    Widget productPreview() {
+      return Container(
+        width: 225,
+        height: 74,
+        margin: EdgeInsets.only(bottom: 20),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: bgColor6,
+          borderRadius: BorderRadius.circular(
+            12,
+          ),
+          border: Border.all(color: primaryColor),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                'assets/image_shoes.png',
+                width: 54,
+              ),
+            ),
+            SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'COURT VISIO...',
+                    style: primaryTextStyle,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 2),
+                  Text(
+                    '\$57,57',
+                    style: priceTextStyle.copyWith(fontWeight: medium),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'assets/button_close.png',
+              width: 22,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget chatInput() {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            productPreview(),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 45,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      color: bgColor4,
+                      borderRadius: BorderRadius.circular(
+                        12,
+                      ),
+                    ),
+                    child: Center(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Type Message...',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Image.asset(
+                  'assets/button_send.png',
+                  width: 45,
+                )
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: bgColor3,
+      appBar: header(),
+      bottomNavigationBar: chatInput(),
+    );
+  }
+}
