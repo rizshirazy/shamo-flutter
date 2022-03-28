c9ea5b1e3da88de469422e14d13b876c4c715b43
Muhammad Rizki <rizkielshirazy@gmail.com>
2022-03-28 09:30:48 +0700

    add new arrivals on home page and navigation to product page

---
+++ lib/pages/product_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Product Page')),
+    );
+  }
+}
