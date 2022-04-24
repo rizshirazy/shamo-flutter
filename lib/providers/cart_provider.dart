ce8d4436770c8cd49ffe4a8b46f5fb32bfd58fef
Muhammad Rizki <rizkielshirazy@gmail.com>
2022-04-24 11:19:44 +0700

    add cart provider and cart model

---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/models/cart_model.dart';
+import 'package:shamo/models/product_model.dart';
+
+class CartProvider with ChangeNotifier {
+  List<CartModel> _carts = [];
+
+  List<CartModel> get carts => _carts;
+
+  set carts(List<CartModel> carts) {
+    _carts = carts;
+    notifyListeners();
+  }
+
+  addCart(ProductModel product) {
+    if (productExist(product)) {
+      int index =
+          _carts.indexWhere((element) => element.product.id == product.id);
+
+      _carts[index].quantity++;
+    } else {
+      _carts.add(
+        CartModel(
+          _carts.length,
+          product,
+          1,
+        ),
+      );
+    }
+
+    notifyListeners();
+  }
+
+  removeCart(int id) {
+    _carts.removeAt(id);
+    notifyListeners();
+  }
+
+  addQuantity(int id) {
+    _carts[id].quantity++;
+    notifyListeners();
+  }
+
+  reduceQuantity(int id) {
+    _carts[id].quantity--;
+
+    if (_carts[id].quantity == 0) {
+      removeCart(id);
+    }
+
+    notifyListeners();
+  }
+
+  int totalItems() {
+    int total = 0;
+    for (var items in _carts) {
+      total += items.quantity;
+    }
+    return total;
+  }
+
+  double totalPrice() {
+    double total = 0;
+    for (var item in _carts) {
+      total += (item.quantity * item.product.price);
+    }
+    return total;
+  }
+
+  productExist(ProductModel product) {
+    if (_carts.indexWhere((element) => element.product.id == product.id) ==
+        -1) {
+      return false;
+    }
+    return true;
+  }
+}
